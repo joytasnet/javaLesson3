623b3955100eec5f919f64c71b159fa572e6c1e0
mjpurin <mjpurin@gmail.com>
2022-03-17 11:04:08 +0900

    Rpg.java追加

--- day2/Rpg.java
+++ day2/Rpg.java
@@ -1,18 +1,19 @@
+import static java.lang.System.*;
 public class Rpg{
 	public static void main(String[] args){
 		String name="ロト";
 		int hp=100;
-		System.out.println("スライムが出てきた！");
-		System.out.println("スライムの先制攻撃");
-		System.out.println(name + "は30のダメージを受けた");
+		out.println("スライムが出てきた！");
+		out.println("スライムの先制攻撃");
+		out.println(name + "は30のダメージを受けた");
 		hp = hp - 30;
-		System.out.println(name+"(" + hp +")");
-		System.out.println("スライムの連続攻撃");
-		System.out.println(name + "は100のダメージを受けた");
+		out.println(name+"(" + hp +")");
+		out.println("スライムの連続攻撃");
+		out.println(name + "は100のダメージを受けた");
 		hp = hp - 100;
-		System.out.println(name+"(" + hp +")");
-		System.out.println(name+"は力尽きた");
-		System.out.println("GameOver");
+		out.println(name+"(" + hp +")");
+		out.println(name+"は力尽きた");
+		out.println("GameOver");
 
 	}
 }
