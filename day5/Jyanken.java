1b9d1025dcba9361e96b932dc6e073c5a0081fd6
mjpurin <mjpurin@gmail.com>
2022-03-25 09:02:24 +0900

    day6追加

---
+++ day5/Jyanken.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Jyanken{
+	public static void main(String[] args){
+		/*
+		 *手を入力(グー:0,チョキ:1,パー:2)>>1
+		 あなたはチョキ、PCはパー
+		 あなたの勝ちです。(負け,あいこ)
+		 * 
+		 */ 
+	}
+}
